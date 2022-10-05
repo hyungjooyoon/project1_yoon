d46f797745b1951c5979d095862f1350d5d03216
HyungJoo Yoon <hyungjooyoon@gmail.com>
2022-10-05 15:01:25 -0700

    Initial server setup with javalin to say hello!

--- src/main/java/com/revature/App.java
+++ src/main/java/com/revature/App.java
@@ -1,5 +1,7 @@
 package com.revature;
 
+import io.javalin.Javalin;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Javalin app = Javalin.create().start(7070);
+        app.get("/", ctx -> ctx.result("Hello!") );
     }
 }
